ed33cec9f5e04ce136ac645d7de5d2af7f5e9db6
vonhu <vonhu.alien@gmail.com>
2023-02-24 21:45:10 +0700

    fix: https

--- layout/EmptyLayout.tsx
+++ layout/EmptyLayout.tsx
@@ -9,7 +9,7 @@ export function EmptyLayout({ children }: LayoutProps) {
 			<Seo
 				title="Đăng ký - Đăng nhập"
 				description="Website dành cho sinh viên trường Đại học Sài Gòn trao đổi hoặc mua bán tài liệu học tập"
-				url="exchange-document-fe.vercel.app"
+				url="https://exchange-document-fe.vercel.app"
 				thumbnailUrl="https://drive.google.com/file/d/1Atx_f0i5laHbjXQeqtBj-TfbyODPtjI5/view"
 			/>
 			<div>{children}</div>
