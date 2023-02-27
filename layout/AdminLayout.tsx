ea557545b625bdcf3fd7878e18b0640b12202759
vonhu <vonhu.alien@gmail.com>
2023-02-27 15:51:54 +0700

    feat: create UI for Admin

--- layout/AdminLayout.tsx
+++ layout/AdminLayout.tsx
@@ -1,57 +1,242 @@
 import { Seo } from '@/components'
-import { Auth } from '@/components/common'
 import { useAuth } from '@/hooks'
 import { LayoutProps } from '@/models'
+import {
+	DashboardOutlined,
+	DownOutlined,
+	FileDoneOutlined,
+	FormOutlined,
+	AppstoreAddOutlined,
+	MenuFoldOutlined,
+	MenuUnfoldOutlined,
+	PieChartOutlined,
+	SmileOutlined,
+	TeamOutlined,
+} from '@ant-design/icons'
+import { Button, Dropdown, Layout, Menu, MenuProps, Space, Typography, message, theme } from 'antd'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
 
-export interface IAdminLayoutProps {}
+import { Auth } from '@/components/common'
+import styles from '../styles/Layout.module.css'
 
-export function AdminLayout({ children }: LayoutProps) {
+const { Header, Content, Footer, Sider } = Layout
+type MenuItem = Required<MenuProps>['items'][number]
+
+function getItem(
+	label: React.ReactNode,
+	key: React.Key,
+	icon?: React.ReactNode,
+	children?: MenuItem[]
+): MenuItem {
+	return {
+		key,
+		icon,
+		children,
+		label,
+	} as MenuItem
+}
+
+export const AdminLayout = ({ children }: LayoutProps): React.ReactElement => {
 	const router = useRouter()
 	const { profile, logout } = useAuth({ revalidateOnMount: false })
+	// STATE OF ANTD
+	const [isSending, setIsSending] = useState<boolean>(false)
+	const [collapsed, setCollapsed] = useState(false)
+	const {
+		token: { colorBgContainer },
+	} = theme.useToken()
+	// STATR CUSTOM
+	type CurrentItem = {
+		index: number
+		key: string
+	}
+	const [currentMenuItem, setCurrentMenuItem] = useState<CurrentItem>({
+		index: 0,
+		key: '',
+	})
+	// MENU LIST
+	const itemsMenu: MenuItem[] = [
+		getItem(
+			'Dashboard',
+			'/admin',
+			<Link href="/admin" passHref>
+				<DashboardOutlined />
+			</Link>
+		),
+		getItem(
+			'Quản lý bài đăng',
+			'/admin/post',
+			<Link href="/admin/post" passHref>
+				<FormOutlined />
+			</Link>
+		),
+		getItem(
+			'Quản lý danh mục',
+			'/admin/category',
+			<Link href="/admin/category" passHref>
+				<AppstoreAddOutlined />
+			</Link>
+		),
+		getItem(
+			'Quản lý thành viên',
+			'/admin/member',
+			<Link href="/admin/member" passHref>
+				<TeamOutlined />
+			</Link>
+		),
+		getItem(
+			'Xem hóa đơn',
+			'/admin/bill',
+			<Link href="/admin/bill" passHref>
+				<FileDoneOutlined />
+			</Link>
+		),
+		getItem(
+			'Thống kê',
+			'/admin/analytic',
+			<Link href="/admin/analytic" passHref>
+				<PieChartOutlined />
+			</Link>
+		),
+	]
+	const items: MenuProps['items'] = [
+		{
+			key: '1',
+			label: (
+				<Button
+					disabled={isSending}
+					onClick={handleLogoutClick}
+					style={{ width: '100%', border: 'none' }}
+				>
+					<SmileOutlined />
+					Logout
+				</Button>
+			),
+		},
+	]
+	// USEEFFECT
+	useEffect(() => {
+		if (window.innerWidth < 992) setCollapsed(true)
+		else setCollapsed(false)
+	}, [])
 
+	useEffect(() => {
+		for (let i = 0; i < itemsMenu.length; i++) {
+			if (itemsMenu[i]?.key === router.asPath) {
+				setCurrentMenuItem({
+					index: i,
+					key: `${itemsMenu[i]?.key}`,
+				})
+				return
+			}
+		}
+	}, [router.asPath])
+	// HANDLE EVENT
 	async function handleLogoutClick() {
 		try {
 			await logout()
+			setIsSending(true)
+			logout()
+			message.success({
+				content: 'Logout successfully',
+				duration: 3,
+			})
+			setIsSending(false)
 			console.log('Redirect to login page')
-			router.push('/login')
+			// router.push('/login')
 		} catch (error) {
 			console.log('Logout error: ' + error)
 		}
 	}
 
 	return (
-		<Auth>
+		<>
 			<Seo
 				title="Admin - Quản trị viên"
 				description="Website dành cho sinh viên trường Đại học Sài Gòn trao đổi hoặc mua bán tài liệu học tập"
 				url="https://exchange-document-fe.vercel.app"
 				thumbnailUrl="https://drive.google.com/file/d/1Atx_f0i5laHbjXQeqtBj-TfbyODPtjI5/view"
 			/>
-			<h1>Admin Layout</h1>
-			<p>Profile: {JSON.stringify(profile)}</p>
-			<div>sidebar</div>
-			<button onClick={handleLogoutClick}>Logout</button>
-			<ul>
-				<li>
-					<Link href="/admin">Dashboard</Link>
-				</li>
-				<li>
-					<Link href="/admin/account">Account</Link>
-				</li>
-				<li>
-					<Link href="/admin/slider">Slider</Link>
-				</li>
-				<li>
-					<Link href="/admin/post">Post</Link>
-				</li>
-				<li>
-					<Link href="/admin/bill">Bill</Link>
-				</li>
-			</ul>
-
-			<body className="mx-32">{children}</body>
-		</Auth>
+			<Layout style={{ minHeight: '100vh' }}>
+				<Sider
+					trigger={null}
+					collapsible
+					collapsed={collapsed}
+					breakpoint="lg"
+					width="230"
+					onBreakpoint={(broken: boolean): void => {
+						if (broken) setCollapsed(true)
+						else setCollapsed(false)
+					}}
+					style={{ minHeight: '100vh', overflowY: 'auto' }}
+				>
+					<Typography.Title
+						level={5}
+						style={{
+							height: '32px',
+							margin: '16px',
+							backgroundColor: 'rgba(255, 255, 255, 0.3)',
+							color: '#fff',
+							fontSize: '14px',
+							textAlign: 'center',
+							lineHeight: '32px',
+							overflow: 'hidden',
+							textOverflow: 'ellipsis',
+						}}
+					>
+						Management
+					</Typography.Title>
+					<Menu
+						theme="dark"
+						mode="inline"
+						selectedKeys={[`${currentMenuItem.key}`]}
+						defaultSelectedKeys={[`${currentMenuItem.key}`]}
+						items={itemsMenu}
+					/>
+				</Sider>
+				<Layout>
+					<Header
+						style={{
+							padding: '0 16px 0 0',
+							background: colorBgContainer,
+							display: 'flex',
+							alignItems: 'center',
+							justifyContent: 'space-between',
+						}}
+					>
+						{React.createElement(collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
+							className: styles['trigger'],
+							onClick: () => setCollapsed(!collapsed),
+						})}
+						<Dropdown menu={{ items }}>
+							<a onClick={(e) => e.preventDefault()}>
+								<Space>
+									Welcome to system
+									<DownOutlined />
+								</Space>
+							</a>
+						</Dropdown>
+					</Header>
+					<Content
+						style={{
+							margin: '24px 16px',
+							padding: 24,
+							minHeight: 280,
+							background: colorBgContainer,
+						}}
+					>
+						<Auth>
+							<div>{children}</div>
+						</Auth>
+					</Content>
+					<Footer style={{ textAlign: 'center' }}>
+						Copyright &#169; {new Date().getFullYear()}. Powered by Vo Nhu and Minh Thuan. All right
+						reserved.
+					</Footer>
+				</Layout>
+			</Layout>
+		</>
 	)
 }
--- pages/admin/post/index.tsx
+++ pages/admin/post/index.tsx
@@ -1,13 +1,115 @@
 import { useAuth } from '@/hooks'
 import { AdminLayout } from '@/layout'
 import { NextPageWithLayout } from '@/models'
-import React from 'react'
+import { Table } from 'antd'
+import { ColumnsType } from 'antd/es/table'
+import type { TableProps } from 'antd/es/table'
 
-type Props = {}
+interface DataType {
+	key: React.Key
+	name: string
+	age: number
+	address: string
+}
 
-const PostAdminPage: NextPageWithLayout = (props: Props) => {
+const columns: ColumnsType<DataType> = [
+	{
+		title: 'Name',
+		dataIndex: 'name',
+		filters: [
+			{
+				text: 'Joe',
+				value: 'Joe',
+			},
+			{
+				text: 'Category 1',
+				value: 'Category 1',
+			},
+			{
+				text: 'Category 2',
+				value: 'Category 2',
+			},
+		],
+		filterMode: 'tree',
+		filterSearch: true,
+		// onFilter: (value: string, record) => record.name.startsWith(value),
+		onFilter: (value: string | number | boolean, record) => {
+			if (typeof value === 'string') {
+				return record.name.startsWith(value)
+			}
+			return false
+		},
+		width: '30%',
+	},
+	{
+		title: 'Age',
+		dataIndex: 'age',
+		sorter: (a, b) => a.age - b.age,
+	},
+	{
+		title: 'Address',
+		dataIndex: 'address',
+		filters: [
+			{
+				text: 'London',
+				value: 'London',
+			},
+			{
+				text: 'New York',
+				value: 'New York',
+			},
+		],
+		// onFilter: (value: string, record) => record.address.startsWith(value),
+		onFilter: (value: string | number | boolean, record) => {
+			if (typeof value === 'string') {
+				return record.name.startsWith(value)
+			}
+			return false
+		},
+		filterSearch: true,
+		width: '40%',
+	},
+]
+
+const data: DataType[] = [
+	{
+		key: '1',
+		name: 'John Brown',
+		age: 32,
+		address: 'New York No. 1 Lake Park',
+	},
+	{
+		key: '2',
+		name: 'Jim Green',
+		age: 42,
+		address: 'London No. 1 Lake Park',
+	},
+	{
+		key: '3',
+		name: 'Joe Black',
+		age: 32,
+		address: 'Sydney No. 1 Lake Park',
+	},
+	{
+		key: '4',
+		name: 'Jim Red',
+		age: 32,
+		address: 'London No. 2 Lake Park',
+	},
+]
+
+const onChange: TableProps<DataType>['onChange'] = (pagination, filters, sorter, extra) => {
+	console.log('params', pagination, filters, sorter, extra)
+}
+
+const PostAdminPage: NextPageWithLayout = (): React.ReactElement => {
 	const { profile } = useAuth()
-	return <div>PostAdminPage: {JSON.stringify(profile)}</div>
+	return (
+		<div>
+			{/* PostAdminPage: {JSON.stringify(profile)} */}
+			<Table columns={columns} dataSource={data} onChange={onChange} />;
+		</div>
+	)
 }
 
 PostAdminPage.Layout = AdminLayout
